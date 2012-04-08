e61bd07cef2db5277ab68b675612b18b09dd8d3a
henix <shellpick@gmail.com>
2012-04-09 00:06:01 +0800

    add parseArgs()

--- flower.js
+++ flower.js
@@ -9,11 +9,15 @@ var Flower = {version:0.1};
 (function(global) {
 "use strict";
 
+#include "parseArgs.js"
+
 #include "csser.js"
 #include "findPos.js"
 #include "eventer.js"
 
 #include "detectIE.js"
+
+global.parseArgs = parseArgs;
 
 global.csser = csser;
 global.findPos = findPos;
