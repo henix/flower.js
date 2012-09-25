f7ac0ceea90064cdd994f4e3d7a9ad54f1d410c4
henix <shellpick@gmail.com>
2012-09-25 19:59:35 +0800

    use assert for cookie

--- cookie.js
+++ cookie.js
@@ -4,8 +4,16 @@
  * http://www.w3schools.com/js/js_cookies.asp
  */
 
-var cookie = {
+#include "assert.js"
 
+var TimeUnits = {};
+TimeUnits.MILLISECONDS = 1;
+TimeUnits.SECONDS = 1000 * TimeUnits.MILLISECONDS;
+TimeUnits.MINUTES = 60 * TimeUnits.SECONDS;
+TimeUnits.HOURS = 60 * TimeUnits.MINUTES;
+TimeUnits.DAYS = 24 * TimeUnits.HOURS;
+
+var cookie = {
 	dict: {},
 
 	/**
@@ -26,14 +34,18 @@ var cookie = {
 	/**
 	 * @param name must not have '=' or ';'
 	 */
-	set: function(name, value, days, path) {
+	set: function(name, value, ms, path) {
+		assert.present(name);
+		assert.isTrue(name.indexOf('=') === -1, 'name must not contains "="');
+		assert.isTrue(name.indexOf(';') === -1, 'name must not contains ";"');
+
 		function myescape(str) {
 			return str.replace(/%/g, '%25').replace(/;/g, '%3B');
 		}
 		var expires;
 		if (days) {
 			var date = new Date();
-			date.setTime(date.getTime()+(days*24*60*60*1000));
+			date.setTime(date.getTime() + ms);
 			expires = "; expires=" + date.toGMTString();
 		} else {
 			expires = "";
