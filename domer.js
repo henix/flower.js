337d08bf8e8e3624e0301ec82a44ec716391d36a
henix <shellpick@gmail.com>
2012-12-17 17:32:33 +0800

    move findPos to Flower.domer.offset

--- domer.js
+++ domer.js
@@ -12,6 +12,29 @@ var domer = {
 			return element.getAttribute(name) !== null;
 		}
 	},
+	offset: function (obj) {
+		if (typeof obj.getBoundingClientRect !== "undefined") {
+			// https://github.com/jquery/jquery/blob/master/src/offset.js
+			var rect = obj.getBoundingClientRect();
+			var docElem = document.documentElement;
+			return {
+				left: rect.left + (window.pageXOffset || docElem.scrollLeft) - (docElem.clientLeft || 0),
+				top: rect.top + (window.pageYOffset || docElem.scrollTop) - (docElem.clientTop || 0)
+			};
+		} else {
+			// http://www.quirksmode.org/js/findpos.html
+			var curleft = 0;
+			var curtop = 0;
+			if (obj.offsetParent) {
+				do {
+					curleft += obj.offsetLeft;
+					curtop += obj.offsetTop;
+					obj = obj.offsetParent;
+				} while(obj);
+			}
+			return {'left':curleft,'top':curtop};
+		}
+	},
 	viewWidth: function() {
 		return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth || 0;
 	},
